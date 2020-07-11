56b00e507b6e217e1b6f9f236f8d02709747f2a7
Fabio Polanco <57909981+fabiopolancoe@users.noreply.github.com>
2020-07-11 18:47:35 -0500

    Update index.php

--- middle-connector/index.php
+++ middle-connector/index.php
@@ -1,39 +1,41 @@
 <?php 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    /*
-    $dbh = pg_connect("host=localhost dbname=test user=postgres");
-
+    
+    $dbh = pg_connect("host=localhost dbname=data user=postgres");
+    
     if (!$dbh) {
-
+        
         die("Error in connection: " . pg_last_error());
-
+        
     }
-
-    $sql = "SELECT * FROM Countries";
-
+    
+    $sql = "SELECT * FROM miembros;";
+    
     $result = pg_query($dbh, $sql);
-
+    
     if (!$result) {
-
+        
         die("Error in SQL query: " . pg_last_error());
-
+        
     }
-
-    while ($row = pg_fetch_array($result)) {
-
-        echo "Country code: " . $row[0] . "<br />";
-
-        echo "Country name: " . $row[1] . "<p />";
-
-    }
-
+    
     pg_free_result($result);
-
-    pg_close($dbh);*/
-
+    
     foreach ($_POST as $value) {
-        echo $value;
+        $post = $value;
     }
+    
+    $sql = "INSERT INTO miembros(membername) VALUES (" . $post . ");";
+    
+    $result = pg_query($dbh, $sql);
+    
+    if (!$result) {
+        
+        die("Error in SQL query: " . pg_last_error());
+        
+    }
+    
+    pg_close($dbh);
 }
+
 ?>
-<!-- Con una librería llamada PDO PostgreSQL -->
